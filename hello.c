f59c0df2147678eaaacd169ab30d35d858ecf65d
ali asad <aliasad059@gmail.com>
2021-10-07 14:15:01 +0330

    add helloWorld process

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+    printf(1,"Hi There!\n");
+    getHelloWorld();
+    printf(1, "succeeded!\n");
+    exit();
+    return 0;
+}
