b7e5ef478f84f8245960a1c59bf1b14fa670d349
ali asad <aliasad059@gmail.com>
2022-01-24 15:42:34 +0330

    add test for round robin policy

--- proc.c
+++ proc.c
@@ -932,3 +932,69 @@ updateTimes()
   }
   release(&ptable.lock);
 }
+
+// Wait for a child process to exit and return its pid.
+// Return -1 if this process has no children. 
+// Init the input variables with the process's valuse
+int
+wait_and_init(int *ct, int *rt, int *wt, int *st, int *tt, int *priority)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      if(p->parent != curproc)
+        continue;
+      
+      if (p->threads_count < 0)
+        continue;
+
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        *ct = p->ct;
+        *rt = p->rt;
+        *wt = p->wt;
+        *st = p->st;
+        *tt = ticks;
+        *priority = p->priority;
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        
+        if (check_pgdir_share(p))
+          freevm(p->pgdir);
+        
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        
+        // reset stack top pointer and pgdir if it is the parent
+        p->stc_top = -1;
+        p->pgdir = 0;
+        p->threads_count = -1;
+
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed)
+    {
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
---
+++ roundRobinTest.c
@@ -0,0 +1,56 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#define PROC_NUM 10
+
+int printReport(int pid[PROC_NUM], int cts[PROC_NUM], int rts[PROC_NUM], int wts[PROC_NUM], int sts[PROC_NUM], int tts[PROC_NUM])
+{
+    int bt_avg = 0, tat_avg = 0, wt_avg = 0;
+    printf(1, "PID\tCBT\tWT\tTAT\n");
+    for (int i = 0; i < PROC_NUM; i++)
+    {
+        int bt = 0, tat = 0, wt = 0;
+        bt = rts[i];
+        tat = tts[i] - cts[i];
+        wt = wts[i];
+        printf(1, "|%d|%d|%d|%d|\n", pid[i], bt, wt, tat);
+        bt_avg += bt;
+        tat_avg += tat;
+        wt_avg += wt;
+    }
+    printf(1, "AVG CBT\tAVG WT\tAVG TAT\n");
+    printf(1, "|%d|%d|%d|\n", bt_avg / PROC_NUM, wt_avg / PROC_NUM, tat_avg / PROC_NUM);
+}
+
+int doSomeWork(int pid)
+{
+    for (int i = 1; i <= 1000; i++)
+    {
+        printf(1, "/%d/ : /%d/\n", pid, i);
+    }
+}
+
+int main()
+{
+    int pid[PROC_NUM], cts[PROC_NUM], rts[PROC_NUM], wts[PROC_NUM], sts[PROC_NUM], tts[PROC_NUM];
+
+    // changePolicy(1);
+
+    for (int i = 0; i < PROC_NUM; i++)
+    {
+        if (fork() == 0)
+        {
+            doSomeWork(getpid());
+            exit();
+        }
+    }
+    for (int i = 0; i < PROC_NUM; i++)
+    {
+        pid[i] = wait_and_init(&cts[i], &rts[i], &wts[i], &sts[i], &tts[i]);
+    }
+
+    printReport(pid, cts, rts, wts, sts, tts);
+
+    exit();
+    return 0;
+}
